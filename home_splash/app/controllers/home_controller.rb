c93ef82292bd3964b8a60a159e66c047311f3eb8
Rachel Trantham <rachelleecampbell@gmail.com>
2015-03-03 09:40:12 -0600

    Created lovely css and cover photo

--- home_splash/app/controllers/home_controller.rb
+++ home_splash/app/controllers/home_controller.rb
@@ -1,19 +1,21 @@
 class HomeController < ApplicationController
   def index
+  
     right_now = Time.now
-    @current_time = right_now.to_formatted_s(:short)
-    # Adjust message by time of day
+    @current_time = right_now.stamp_like("Feb 23 2015, 02:00 PM")
     
-
+# remember to finish the string with .html_safe
       @greet_message = case right_now.hour
       when 5..11
-        "Good Morning!"
+        "Good Morning! <i class='fa fa-bicycle'></i>".html_safe
       when 12..15
-        "Good Afternoon!"
+        "Good Afternoon! <i class='fa fa-beer'></i>".html_safe
       when 16..19
-        "Good Evening"
+        "Good Evening! <i class='fa fa-beer'></i>".html_safe
       when 20..23, 0..4
-        "Good Night"
+        "Good Night! Early to bed early to shred <i class='fa fa-bed'></i>".html_safe
       end 
   end
 end
+
+
